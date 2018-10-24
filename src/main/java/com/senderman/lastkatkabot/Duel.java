7037686a669f279b9fe02cd1ba134ada4ce0aa65
Senderman <doletov.fyodor@yandex.ru>
2018-10-24 23:52:30 +0300

    End

--- src/main/java/com/senderman/lastkatkabot/Duel.java
+++ src/main/java/com/senderman/lastkatkabot/Duel.java
@@ -100,8 +100,10 @@ public class Duel {
             initStats(id);
         }
         var updateDoc = new Document()
-                .append("$inc", new Document("total", 1))
                 .append("$inc", new Document("wins", 1));
+        duelstats.updateOne(Filters.eq("id", id), updateDoc);
+        updateDoc = new Document()
+                .append("$inc", new Document("total", 1));
         duelstats.updateOne(Filters.eq("id", id), updateDoc);
     }
 
