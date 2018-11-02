00cb4837b1849aa117025a79903e7f9c815a9c83
Senderman <doletov.fyodor@yandex.ru>
2018-11-02 13:02:09 +0300

    Amend

--- src/main/java/com/senderman/lastkatkabot/commandhandlers/UsercommandsHandler.java
+++ src/main/java/com/senderman/lastkatkabot/commandhandlers/UsercommandsHandler.java
@@ -109,7 +109,7 @@ public class UsercommandsHandler {
             .append(name)
             .append("</a>\n\n")
             .append(text.replace("/feedback", ""));
-        handler.sendMessage((long) LastKatkaBotHandler.mainAdmin, sb.toString());
+        handler.sendMessage((long) LastkatkaBotHandler.mainAdmin, sb.toString());p
     }
 
     public void help() { // /help
