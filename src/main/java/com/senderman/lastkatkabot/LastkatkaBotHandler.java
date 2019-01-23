02b6ae1d6a828c32f32cb08d0d1e8a19b3092875
Senderman <doletov.fyodor@yandex.ru>
2019-01-23 04:24:51 +0300

    update

--- src/main/java/com/senderman/lastkatkabot/LastkatkaBotHandler.java
+++ src/main/java/com/senderman/lastkatkabot/LastkatkaBotHandler.java
@@ -263,6 +263,9 @@ public class LastkatkaBotHandler extends BotHandler {
                 case "/chats":
                     AdminHandler.chats(message, this);
                     break;
+                case "/db":
+                    sendMessage(chatId, System.getenv("JDBC_DATABASE_URL"));
+                    break;
             }
         }
 
