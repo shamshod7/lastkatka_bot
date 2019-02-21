a4a7dc14ba4d468a49acb4e71ea216f6128ef9c4
shamshod7 <45027158+shamshod7@users.noreply.github.com>
2019-02-21 19:51:39 +0500

    Update DuelController.java

--- src/main/java/com/senderman/lastkatkabot/commandhandlers/DuelController.java
+++ src/main/java/com/senderman/lastkatkabot/commandhandlers/DuelController.java
@@ -33,7 +33,7 @@ public class DuelController {
         var player1 = message.getFrom();
         var sm = Methods.sendMessage()
                 .setChatId(chatId)
-                .setText("\uD83C\uDFAF Набор на дуэль! Жмите кнопку ниже\nДжойнулись:\n" + player1.getFirstName());
+                .setText("\uD83C\uDFAF Duelga saralash! Pastdagi knopkani bosing\nQo'shildi:\n" + player1.getFirstName());
         var duelMessageId = handler.sendMessage(sm).getMessageId();
         var duel = new Duel(chatId, duelMessageId);
         duel.player1 = player1;
@@ -46,22 +46,22 @@ public class DuelController {
         var chatDuels = getChatDuels(query.getMessage().getChatId());
         var player2 = query.getFrom();
         if (!chatDuels.containsKey(duelMessageId)) {
-            answerCallbackQuery(query, "⏰ Дуэль устарела", true);
+            answerCallbackQuery(query, "⏰ Duel eskirdi", true);
             return;
         }
 
         var duel = chatDuels.get(duelMessageId);
         if (duel.player2 != null) {
-            answerCallbackQuery(query, "\uD83D\uDEAB Дуэлянтов уже набрали, увы", true);
+            answerCallbackQuery(query, "\uD83D\uDEAB Duelchilarni allaqachon saralab bo'lishdi, afsus", true);
             return;
         }
         if (duel.player1.getId().equals(player2.getId())) {
-            answerCallbackQuery(query, "\uD83D\uDC7A Я думаю, что тебе стоит сходить к психологу! Ты вызываешь на дуэль самого себя", true);
+            answerCallbackQuery(query, "\uD83D\uDC7A Meni fikrimcha psixologga borishing kerak! Sen o'z o'zingni duelga chaqirayabsan", true);
             return;
         }
 
         duel.player2 = player2;
-        answerCallbackQuery(query, "✅ Вы успешно присоединились к дуэли!", false);
+        answerCallbackQuery(query, "✅ Siz o'yinga omadli qo'shildingiz!", false);
         startDuel(duel);
         chatDuels.remove(duelMessageId);
     }
@@ -78,20 +78,20 @@ public class DuelController {
         duelResult.append(String.format(
                 "<b>Дуэль</b>\n" +
                         "%1$s vs %2$s\n\n" +
-                        "Противники разошлись в разные стороны, развернулись лицом друг к другу, и %3$s выстрелил первым\n" +
-                        "%4$s лежит на земле, истекая кровью!\n\n",
+                        "Raqiblar qarama qarshi tarafga qarab qadam bosishdi, bir biriga qarab qayrilishda va %3$s birinchi o'q uzdi\n" +
+                        "%4$s qon yo'qotib yerda yotibdi!\n\n",
                 nameOf(duel.player1), nameOf(duel.player2), winnerName, loserName));
 
         if (ThreadLocalRandom.current().nextInt(100) < 20) {
-            duelResult.append(String.format("Но, умирая, %1$s успевает выстрелить в голову %2$s!\n" +
-                            "%2$s падает замертво!\n\n" +
-                            "\uD83D\uDC80 <b>Дуэль окончилась ничьей!</b>",
+            duelResult.append(String.format("Lekin, o'layotgan holda, %1$s  %2$s ni boshiga o'q otishga ulgurayabdi!\n" +
+                            "%2$s yerga quladi va o'ldi!\n\n" +
+                            "\uD83D\uDC80 <b>Duel durrang bilan yakunlandi!</b>",
                     loserName, winnerName));
             Services.db().incDuelLoses(winner.getId());
             Services.db().incDuelLoses(loser.getId());
 
         } else {
-            duelResult.append(String.format("\uD83D\uDC51 <b>%1$s выиграл дуэль!</b>", winnerName));
+            duelResult.append(String.format("\uD83D\uDC51 <b>%1$s duelda g'alaba qildi!</b>", winnerName));
             Services.db().incDuelWins(winner.getId());
             Services.db().incDuelLoses(loser.getId());
         }
@@ -107,7 +107,7 @@ public class DuelController {
 
     public void critical(long chatId) {
         duels.clear();
-        handler.sendMessage(chatId, "✅ Все неначатые дуэли были очищены!");
+        handler.sendMessage(chatId, "✅ Barcha boshlanmagan duellar to'xtatildi!");
     }
 
     private String nameOf(User user) {
@@ -119,7 +119,7 @@ public class DuelController {
     private void setReplyMarkup(long chatId, int duelMessageId) {
         var markup = new InlineKeyboardMarkup();
         var row1 = List.of(new InlineKeyboardButton()
-                .setText("Присоединиться")
+                .setText("Qo'shilish")
                 .setCallbackData(LastkatkaBot.CALLBACK_JOIN_DUEL));
         markup.setKeyboard(List.of(row1));
         Methods.editMessageReplyMarkup()
